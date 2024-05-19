a82dfeb101332685a4bd365ddb3742f6388077f2
glnnluciano <glnnluciano@gmail.com>
2024-05-20 06:48:18 +0800

    call set/get volume methods

--- testTV.py
+++ testTV.py
@@ -10,9 +10,15 @@ tv2 = TV()
 tv1.set_channel(2)
 tv2.set_channel(5)
 
+tv1.set_volume(3)
+tv2.set_volume(6)
+
 get_channel1 = tv1.get_channel()
 get_channel2  = tv2.get_channel()
 
+get_volume1 = tv1.get_volume()
+get_volume2 = tv2.get_volume()
+
 # print result
-print(get_channel1)
-print(get_channel2)
+print("TV1 channel:", get_channel1, "volume:", get_volume1 )
+print("TV2 channel:", get_channel2, "volume:", get_volume2)
