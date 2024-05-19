ca55f0f78bdf50d64e1d108737e93a0dfc597cf6
glnnluciano <glnnluciano@gmail.com>
2024-05-20 05:14:27 +0800

    create class

---
+++ TV.py
@@ -0,0 +1,28 @@
+# TV class
+class TV:
+# define __init__ attributes and default values of attributes
+    def __init__(self):
+        # set 1 as default channel
+        self.channel = 1
+        # set 1 as default volume_level
+        self.volume_level = 1
+        # set false (off) as default
+        self.power_on = False
+        
+# testTV (new file)
+# import class
+# create objects (tv1 and tv2)
+# define methods
+    # turn_on()
+    # turn_off()
+    # get_channel()
+    # set_channel()
+    # get_volume()
+    # set volume()
+    # channel_up()
+    # channel_down()
+    # volume_up()
+    # volume_down()
+
+# call methods
+# print result
