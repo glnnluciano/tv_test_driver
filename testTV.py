1b90f6afb612c1d5ad6b561d284d16a36ea487a5
glnnluciano <glnnluciano@gmail.com>
2024-05-20 06:59:32 +0800

    add volume/channel up and down method call

--- testTV.py
+++ testTV.py
@@ -13,6 +13,12 @@ tv2.set_channel(5)
 tv1.set_volume(3)
 tv2.set_volume(6)
 
+tv1.channel_down()
+tv2.channel_up()
+
+tv1.volume_up()
+tv2.volume_down()
+
 get_channel1 = tv1.get_channel()
 get_channel2  = tv2.get_channel()
 
@@ -20,5 +26,5 @@ get_volume1 = tv1.get_volume()
 get_volume2 = tv2.get_volume()
 
 # print result
-print("TV1 channel:", get_channel1, "volume:", get_volume1 )
+print("TV1 channel:", get_channel1, "volume:", get_volume1)
 print("TV2 channel:", get_channel2, "volume:", get_volume2)
