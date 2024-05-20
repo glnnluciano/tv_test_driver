789d42c9c56aba2951138099a953d029f9772403
glnnluciano <glnnluciano@gmail.com>
2024-05-20 10:31:58 +0800

    Finalize

--- testTV.py
+++ testTV.py
@@ -10,27 +10,28 @@ tv2 = TV()
 tv1.turn_on()
 tv2.turn_on()
 
-tv1.turn_off()
-tv2.turn_off()
+tv1.set_channel(29)
+tv2.set_channel(4)
 
-tv1.set_channel(1)
-tv2.set_channel(120)
+tv1.set_volume(4)
+tv2.set_volume(1)
 
-tv1.set_volume(3)
-tv2.set_volume(6)
+tv1.channel_up()
+tv2.channel_down()
 
-tv1.channel_down()
-tv2.channel_up()
+tv1.volume_down()
+tv2.volume_up()
 
-tv1.volume_up()
-tv2.volume_down()
+tv1.turn_off
+tv2.turn_off
 
-get_channel1 = tv1.get_channel()
-get_channel2  = tv2.get_channel()
+get_channel_tv1 = tv1.get_channel()
+get_channel_tv2  = tv2.get_channel()
 
-get_volume1 = tv1.get_volume()
-get_volume2 = tv2.get_volume()
+get_volume_tv1 = tv1.get_volume()
+get_volume_tv2 = tv2.get_volume()
+
 
 # print result
-print("TV1 channel:", get_channel1, "volume:", get_volume1)
-print("TV2 channel:", get_channel2, "volume:", get_volume2)
+print("TV1's channel is", get_channel_tv1, "and volume level is", get_volume_tv1)
+print("TV2's channel is", get_channel_tv2, "and volume level is", get_volume_tv2)
